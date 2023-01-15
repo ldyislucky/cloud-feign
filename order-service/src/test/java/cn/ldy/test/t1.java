b0a40ef78d53e5bab4dce6085874024db1ab8e52
ldy <1278950466@qq.com>
2023-01-15 16:49:20 +0800

    添加一个小测试，以后思考一下double精度丢失问题应当如何处理

---
+++ order-service/src/test/java/cn/ldy/test/t1.java
@@ -0,0 +1,19 @@
+package cn.ldy.test;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author : ldy
+ * @version : 1.0
+ */
+public class t1 {
+    @Test
+    public void t2(){
+        int i =10;
+        int j = i/3;
+        System.out.println(j);
+
+        double a = 10;
+        System.out.println(a/3);
+    }
+}
