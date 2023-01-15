cfd18d1c2248803b16e47ef019a95615aebd53aa
ldy <1278950466@qq.com>
2023-01-15 10:39:45 +0800

    完成gateway的启动配置

--- Gateway/src/main/java/cn/ldy/gateway/GatewayApplication.java
+++ Gateway/src/main/java/cn/ldy/gateway/GatewayApplication.java
@@ -10,6 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication//springboot启动注解
 public class GatewayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootApplication.class,args);
+        SpringApplication.run(GatewayApplication.class,args);
     }
 }
